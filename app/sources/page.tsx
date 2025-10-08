944ed651fc4c8fd2860bd044b067831bc3091650
EllisOllier <ellisollier1@gmail.com>
2025-10-08 12:19:11 +0100

    Initial commit

---
+++ app/sources/page.tsx
@@ -0,0 +1,18 @@
+export default function Sources(){
+
+    return (
+        <div>
+            <header>
+                Sources for green computing
+            </header>
+            <main>
+                <p>List of sources</p>
+                <ul>
+                    <li>Source 1</li>
+                    <li>Source 2</li>
+                    <li>Source 3</li>
+                </ul>
+            </main>
+        </div>
+    )
+}
